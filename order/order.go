931efbe2a1f05efc230856318d7941c1c6871326
EdwardLuthor <alexandrobtway@gmail.com>
2025-05-12 16:26:57 +1000

    fixed pizza and order

--- order/order.go
+++ order/order.go
@@ -1,7 +1,5 @@
 package order
 
-import "time"
-
 type OrderState string
 
 const (
@@ -10,10 +8,4 @@ const (
 	OrderStateDone       OrderState = "done"
 )
 
-type Order struct {
-	Items     []pizza.pizza
-	TotalCost float64
-	State     OrderState
-	CreatedAt time.Time
-	UpdatedAt time.Time
-}
+type Order map[string]interface{}
--- pizza/pizza.go
+++ pizza/pizza.go
@@ -1,7 +1,28 @@
 package pizza
 
-type Pizza struct {
-	ID   int
-	Name string
-	Cost float64
+type Pizza map[string]interface{}
+
+var pizzas []Pizza
+
+func ListPizzas() []Pizza {
+	return pizzas
+}
+
+func AddPizza(pizza Pizza) {
+	pizza["ID"] = len(pizzas) + 1
+	pizzas = append(pizzas, pizza)
+}
+
+func RemovePizza(id int) bool {
+	newPizzas := []Pizza{}
+	found := false
+	for _, p := range pizzas {
+		if p["ID"] != id {
+			newPizzas = append(newPizzas, p)
+		} else {
+			found = true
+		}
+	}
+	pizzas = newPizzas
+	return found
 }
