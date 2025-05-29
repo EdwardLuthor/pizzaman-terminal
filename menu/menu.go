83fc0f227c9400cd2bf7d481cbb73ef2f9cb9ea3
EdwardLuthor <alexandrobtway@gmail.com>
2025-05-29 16:40:21 +1000

    v.0.2

--- menu/menu.go
+++ menu/menu.go
@@ -1,66 +1,150 @@
 package menu
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strconv"
+	"strings"
 
 	"github.com/EdwardLuthor/pizzaman-terminal/pizza"
 )
 
-type PizzaManager struct {
-	pizzas []pizza.Pizza
+type Menu struct {
+	pizzaManager *pizza.Manager
+	reader       *bufio.Reader
 }
 
-func NewPizzaManager() PizzaManager {
-	return PizzaManager{pizzas: []pizza.Pizza{}}
+func NewMenu() *Menu {
+	return &Menu{
+		pizzaManager: pizza.NewManager(),
+		reader:       bufio.NewReader(os.Stdin),
+	}
 }
+func (m *Menu) displayMainMenu() {
+	fmt.Println("Выберите действие:")
+	fmt.Println("1. Вывести список пицц")
+	fmt.Println("2. Добавить новую пиццу")
+	fmt.Println("3. Удалить пиццу")
+	fmt.Println("0. Выход")
+
+	fmt.Print("> ")
+}
+
+func (m *Menu) getChoice() (int, error) {
+	var choiceSTR string
+	choiceSTR, err := m.reader.ReadString('\n')
+	if err != nil {
+		return 0, fmt.Errorf("Ошибка чтения выбора: ", err)
+	}
+	choiceSTR = strings.TrimSpace(choiceSTR)
+
+	choice, err := strconv.Atoi(choiceSTR)
+	if err != nil {
+		return 0, fmt.Errorf("Некорректный формат выбора: ", err)
+	}
+	return choice, nil
+}
+func (m *Menu) ListPizzas() {
+
+	pizzas := m.pizzaManager.ListPizzas()
+	fmt.Println("\n--- Меню пицц ---")
+	if len(pizzas) == 0 {
+		fmt.Println("В меню пока нет пицц.")
+	} else {
+		for _, p := range pizzas {
+			fmt.Printf("%d. %s - $%.2f\n", p.ID, p.Name, p.Cost)
+		}
+	}
+	fmt.Println("-------------------\n")
+}
+
+func (m *Menu) getPizzaDetailsFromUser() (name string, cost float64, err error) {
+	fmt.Print("Введите название новой пиццы: ")
+	name, err = m.reader.ReadString('\n')
+	if err != nil {
+		return "", 0, fmt.Errorf("ошибка чтения названия: ", err)
+	}
+	name = strings.TrimSpace(name)
+	if name == "" {
+		return "", 0, fmt.Errorf("название пиццы не может быть пустым")
+	}
+
+	fmt.Print("Введите стоимость новой пиццы: ")
+	costSTR, err := m.reader.ReadString('\n')
+	if err != nil {
+		return "", 0, fmt.Errorf("ошибка чтения стоимости: ", err)
+	}
+	costSTR = strings.TrimSpace(costSTR)
+
+	cost, err = strconv.ParseFloat(costSTR, 64)
+	if err != nil {
+		return "", 0, fmt.Errorf("некорректный формат стоимости. Пожалуйста, введите число.")
+	}
+	return name, cost, nil
+}
+
+func (m *Menu) AddpPizza() {
+	name, cost, err := m.getPizzaDetailsFromUser()
+	if err != nil {
+		fmt.Println("Ошибка добавления пиццы:", err)
+		return
+	}
+
+	pizzaID := m.pizzaManager.AddPizza(name, cost)
+	fmt.Printf("Пицца '%s' добавлена в меню с ID %d.\n\n", name, pizzaID)
+}
+
+func (m *Menu) getPizzaIDFromUser() (id int, err error) {
+	fmt.Print("Введите ID пиццы для удаления: ")
+	idSTR, err := m.reader.ReadString('\n')
+	if err != nil {
+		return 0, fmt.Errorf("ошибка чтения ID: ", err)
+	}
+	idSTR = strings.TrimSpace(idSTR)
+
+	id, err = strconv.Atoi(idSTR)
+	if err != nil {
+		return 0, fmt.Errorf("некорректный формат ID. Пожалуйста, введите число.")
+	}
+	return id, nil
+}
+
+func (m *Menu) RemovePizza() {
+	id, err := m.getPizzaIDFromUser()
+	if err != nil {
+		fmt.Println("Ошибка удаления пиццы:", err)
+		return
+	}
+
+	wasRemoved := m.pizzaManager.RemovePizza(id)
+
+	if wasRemoved {
+		fmt.Printf("Пицца с ID %d успешно удалена.\n\n", id)
+	} else {
+		fmt.Printf("Пицца с ID %d не найдена в меню.\n\n", id)
+	}
+}
+
 func RunMenu() {
-	pizzaManager := NewPizzaManager()
-	for {
-		fmt.Println("Выберите действие:")
-		fmt.Println("1. Вывести список пицц")
-		fmt.Println("2. Добавить новую пиццу")
-		fmt.Println("3. Удалить пиццу")
-		fmt.Println("0. Выход")
 
-		var choice int
-		_, err := fmt.Scanln(&choice)
+	menu := NewMenu()
+
+	for {
+		menu.displayMainMenu()
+		choice, err := menu.getChoice()
 		if err != nil {
-			fmt.Println("Ошибка ввода")
+			fmt.Println("Ошибка", err)
 			continue
 		}
 
 		switch choice {
 		case 1:
-			Manager.ListPizzas()
-
+			menu.ListPizzas()
 		case 2:
-			fmt.Println("Введите название новой пиццы: ")
-			var name string
-			fmt.Scanln(&name)
-			fmt.Println("Введите стоимость новой пиццы: ")
-			var cost float64
-			_, err := fmt.Scanln(&cost)
-			if err != nil {
-				fmt.Println("Ошибка, некорректный формат стоимости")
-				continue
-			}
-			pizzaManager = Manager.AddPizza(name, cost)
+			menu.AddpPizza()
 		case 3:
-			fmt.Print("Введите ID пиццы для удаления: ")
-			var id int
-			var removed bool
-			_, err := fmt.Scanln(&id)
-			if err != nil {
-				fmt.Println("Ошибка, некорректный формат ID")
-				continue
-			}
-			pizzaManager, removed = Manager.RemovePizza(id)
-			// found можно убрать, не имеет особого смысла
-			if removed {
-				fmt.Println("Пицца успешно удалена.")
-			} else {
-				fmt.Println("Пицца с таким ID не найдена. ")
-			}
+			menu.RemovePizza()
 		case 0:
 			fmt.Println("Выход из программы.")
 			return
--- pizza/manager.go
+++ pizza/manager.go
@@ -1,41 +1,34 @@
 package pizza
 
-import "fmt"
-
 type Manager struct {
 	pizzas []Pizza
+	nextID int
 }
 
-func NewManager() Manager {
-	return Manager{pizzas: make([]Pizza, 0)}
+func NewManager() *Manager {
+	return &Manager{pizzas: make([]Pizza, 0),
+		nextID: 1}
 }
 
-func (m Manager) ListPizzas() {
-	fmt.Println("\n Меню пицц ")
-	if len(m.pizzas) == 0 {
-		fmt.Println("В меню пока нет пицц.")
-		return
-	}
-	for _, p := range m.pizzas {
-		fmt.Printf("%d. %s - $%.2f\n", p.ID, p.Name, p.Cost)
-	}
+func (m Manager) ListPizzas() []Pizza {
+	return m.pizzas
 }
 
-func (m Manager) AddPizza(name string, cost float64) Manager {
+func (m *Manager) AddPizza(name string, cost float64) int {
 	newPizza := Pizza{
-		ID:   len(m.pizzas) + 1,
+		ID:   m.nextID,
 		Name: name,
 		Cost: cost,
 	}
-	newManager := Manager{pizzas: append(m.pizzas, newPizza)}
-	fmt.Printf("Пицца %s добавлена в меню с ID %d.\n\n", newPizza.Name, newPizza.ID)
-	return newManager
+	m.pizzas = append(m.pizzas, newPizza)
+	m.nextID++
+	return newPizza.ID
 }
 
-func (m Manager) RemovePizza(id int) (Manager, bool) {
-	initialLen := len(m.pizzas)
+func (m *Manager) RemovePizza(id int) bool {
 	newPizzas := []Pizza{}
 	var found bool = false
+
 	for _, p := range m.pizzas {
 		if p.ID != id {
 			newPizzas = append(newPizzas, p)
@@ -43,11 +36,6 @@ func (m Manager) RemovePizza(id int) (Manager, bool) {
 			found = true
 		}
 	}
-	newManager := Manager{pizzas: newPizzas}
-	if len(newManager.pizzas) < initialLen {
-		fmt.Printf("Пицца с ID %d удалена.\n\n", id)
-	} else {
-		fmt.Printf("Пицца с ID %d не найдена в меню.\n\n", id)
-	}
-	return newManager, found
+	m.pizzas = newPizzas
+	return found
 }
